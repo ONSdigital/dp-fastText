5166ae5e8a101a77997370e177a3b51ba937c7bf
David Sullivan <dsullivan0190@gmail.com>
2018-06-04 09:36:52 +0100

    Updated build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,5 +2,5 @@
 
 pushd dp-fastText
   make build
-  cp -r python lib build_model.py requirements.txt requirements_test.txt Makefile Dockerfile.concourse ../build/
+  cp -r build_model.py tests lib requirements*.txt Makefile Dockerfile.concourse ../build/
 popd
